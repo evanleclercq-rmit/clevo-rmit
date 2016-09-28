880b04706e83d2712130b102b689ef330ae5175c
Lucas Brook <s3229113@student.rmit.edu.au>
2016-09-28 16:23:58 +1000

    corrected typo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Budding Stockmarket Investor
+                    Budding Sharemarket Investor
                 </div>
 
                 <div class="links">
