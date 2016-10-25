5b5576073af90a4f943a96712f2a8a53d2d8826b
Lucas Brook <s3229113@student.rmit.edu.au>
2016-10-25 16:49:35 +1000

    added buying function

--- resources/views/company.blade.php
+++ resources/views/company.blade.php
@@ -1,6 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
+<?php require(app_path().'/financeWebService.php') ?>
+
+		<script type="text/javascript">
+			function calculateTotalShareCostBuy(numShares)
+                {
+                    document.getElementById("totalCostOfSharesBuy").value=numShares.value*document.getElementById("sharePrice").value;
+                }
+				
+			function calculateTotalShareCostSell(numShares)
+                {
+                    document.getElementById("totalCostOfSharesSell").value=numShares.value*document.getElementById("sharePrice").value;
+                }
+			
+		</script>
+
 
 <?php
     require_once('../app/financeWebService.php');
@@ -14,7 +29,7 @@
                     
 	if(isset($_POST)&&!empty($_POST))
 	{
-		$stockSymbol = $_POST['companies'];
+		$stockSymbol = $_POST['symbol'];
 		$stockData = search_stock($stockSymbol);
 		$company = $stockData['name'];
 		$price = $stockData['price'];
@@ -24,7 +39,13 @@
 		$graph = $stockData['graph'];
 
 	}
+	    if(isset($_POST['totalCostOfSharesBuy']))
+        {
+            $user = Auth::user();
+            $user->decrement('balance',$_POST['totalCostOfSharesBuy']);
+        }
 ?>
+
 
 	<div id="body">	
 		<div class="container">
@@ -33,7 +54,7 @@
 					<h3><b>Stockmarket Information</b></h3><br>
 					<div class="form-group">
 					{{ Form::open() }}
-					{{ Form::select('companies', $companies, null, ['placeholder' => 'Select a company...']) }}
+					{{ Form::select('symbol', $companies, null, ['placeholder' => 'Select a company...']) }}
 					{{ Form::button('Submit', array('type' => 'submit', )) }}
 					{{ Form::close() }}
 				</div>
@@ -41,13 +62,7 @@
 				<section>
 
  				<div id="info">
-<!--  					<ul>
-					<li>Company: <?php echo $company ?></li>
-					<li>Price: <?php echo $price ?></li>
-					<li>Currency: <?php echo $currency ?></li>
-					<li>Change: <?php echo $change ?></li>
-					<li>Change from year high: <?php echo $changeFromYearHigh ?></li> 
-					</ul>-->
+
 				<table>
 				  <tr>
 				    <th>Company:</th>
@@ -75,13 +90,46 @@
 				 	<?php echo $graph ?>
 				</div>
  				</section>
-
-				
-
+				<form  name="buySharesForm" action="{{ action('DashboardController@index') }}" method="post">
+                    <input name="searchText" type="hidden" type="text" value="<?php echo isset($_POST['symbol']) ? $_POST['symbol'] : '' ?>">
+					
+					Shares to Buy :<input name="sharePrice" type="hidden" id="sharePrice" value="<?php echo $price ?>" ><br>
+					<input name="numberOfSharesBuy" id="numberOfSharesBuy" style="width: 4.5em" placeholder="#" type="number" min="1" step="1"
+                       onchange="calculateTotalShareCostBuy(this)" disabled><br>
+					Total Value: <input name="totalCostOfSharesBuy" style="width: 6em;" id="totalCostOfSharesBuy" value="" readonly> <?php echo $currency ?><br>
+					
+					<button class="submitButt" id="buySharesButton" type="submit" value="submit" disabled>Buy Shares</button>
+					<br>
+				{{ csrf_field() }}	
+				</form>
 				</div>
 			</div>
 		</div>
 	</div><!--//body-->	
 		
+		<?php
+    
+     if(isset($_POST['symbol']))
+        {
+            echo "<script type=\"text/javascript\"> document.getElementById('numberOfSharesBuy').disabled=false;
+                    document.getElementById('buySharesButton').disabled=false;</script>";
+                
+        }
+        else
+        {
+            echo "";
+        }
+	if(isset($_POST['symbol']))
+        {
+            echo "<script type=\"text/javascript\"> document.getElementById('numberOfSharesSell').disabled=false;
+                    document.getElementById('sellSharesButton').disabled=false;</script>";
+                
+        }
+        else
+        {
+            echo "";
+        }
+	
+?>
 
 @endsection
