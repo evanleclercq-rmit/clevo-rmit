e5f62009b19f2225c1097148c82ff314cbfb6e0d
VIOWINMBP\dashony <s3345368@student.rmit.edu.au>
2016-10-30 19:48:47 +1100

    UI works on User profile page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <h2>Profile<h2>
-			<h2>{{ $user->name }}'s Profile</h2>
-            <h2>{{ $user->email }} </h2>
-		    <h2>{{ $user->balance}} </h2>
-			<h2>{{ $user->bio}} </h2>
-           			
-        
-    </div>
+
+<div id="body">	
+	<div class="container">
+		<div class="col-md-12 content-left">
+			<div class="contact-form wow fadeInUp animated" data-wow-delay=".1s">
+				<h3><b>Profile</b></h3>
+				<p>Name  &nbsp; &nbsp;:&nbsp;{{ $user->name }}</p>
+				<p>Email &nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;{{ $user->email }}</p>
+				<p>Balance :&nbsp;${{ $user->balance}}</p>
+				<p>Bio&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;{{ $user->bio}} </p>
+			</div>
+		</div>
+	</div>
 </div>
-</div>
+
 @endsection
