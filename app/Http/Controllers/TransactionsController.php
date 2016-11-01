a26ffb111b350f162ef17c2b518962dfdbe580ce
Evan Le Clercq <s3516635@student.rmit.edu.au>
2016-11-01 14:18:12 +1030

    Buy and Sell setup

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -4,14 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
+require(app_path().'/DatabaseUtilities');
 
 class TransactionsController extends Controller
 {
-    
+
     // initial landing function for the transactions
     public function index () {
         $companies = \App\Companies::pluck('name', 'symbol');
     	return view('transactions')->with('companies', $companies);
     }
-    
+
+    public function buy () {
+        //TODO: Get code and number to sell
+        //      update database holdings field
+        //      update database balance
+    }
+
+    public function sell () {
+        //TODO: Get code and number to sell
+        //      update database holdings field
+        //      update database balance
+    }
+
 }
--- resources/views/transactions.blade.php
+++ resources/views/transactions.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 
 <?php require(app_path().'/financeWebService.php') ?>
+<?php require(app_path().'/DatabaseUtilities') ?>
 <script type="text/javascript">
 
 <?php
@@ -21,7 +22,7 @@ if(isset($_POST)&&!empty($_POST))
 	$price = $stockData['price'];
 	$currency = $stockData['currency'];
 	$change = $stockData['change'];
-	
+
 
 }
 if(isset($_POST['totalCostOfSharesBuy']))
@@ -36,20 +37,20 @@ var symbolSearch = document.getElementById("symbolSearch").value;
 function processApiData(array)
 {
     var newContent = '<br><li>Company : '+array.Name+'</li><li>Price &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : '+array.Ask+'</li><li>Currency  : '+array.Currency+'</li><li>Change   &nbsp;&nbsp; : '+array.Change+'</li>';
-    
+
     document.getElementById("companyData").innerHTML = newContent;
-    
-    document.getElementById('numberOfSharesBuy').disabled=false;   
+
+    document.getElementById('numberOfSharesBuy').disabled=false;
     document.getElementById('buySharesButton').disabled=false;
     document.getElementById('sharePrice').value = array.Ask;
-    
-    
-    
+
+
+
 }
 
 function ajaxSearch(str)
 {
-    
+
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function()
 	{
@@ -58,7 +59,7 @@ function ajaxSearch(str)
 			//document.getElementById("companyData").innerHTML = this.responseText;
             var json = JSON.parse(this.responseText);
             processApiData(json);
-            
+
 		}
 	};
 	xmlhttp.open("GET", "{{ action('ApiRequestController@index') }}"+"?q=" + str, true);
@@ -99,7 +100,7 @@ $(document).ready(function(){
 });
 </script>
 
-<div id="body">	
+<div id="body">
 	<div class="container">
 		<div class="col-md-12 content-left">
 			<div class="contact-form wow fadeInUp animated">
@@ -114,7 +115,7 @@ $(document).ready(function(){
 				<div class="buy box" style="display:none">
 					<section>
 						<div id="left">
-							<form  name="APIsearchForm" onsubmit="return false";>						
+							<form  name="APIsearchForm" onsubmit="return false";>
 								{{ Form::open() }}
 								{{ Form::select('symbol', $companies, null, ['placeholder' => 'Select a company...', 'onchange' => 'ajaxSearch(this.value);']) }}
 								{{ Form::close() }}
@@ -122,17 +123,17 @@ $(document).ready(function(){
 							</form>
 							<br>
 							<p>Or search by company symbol</p>
-							<form  name="APIsearchForm" onsubmit="return false"> 
-								<input name="symbol" id="symbolSearch" placeholder="eg. ASX.AX" type="text" style="width: 250px" > 
+							<form  name="APIsearchForm" onsubmit="return false">
+								<input name="symbol" id="symbolSearch" placeholder="eg. ASX.AX" type="text" style="width: 250px" >
 								<button class="submitButt" type="submit" onclick="ajaxSearch(symbolSearch.value);" value="submit">Search</button>
 								{{ csrf_field() }}
 							</form>
 						</div>
 
 						<div id="right">
-							<form  name="buySharesForm" action="{{ action('TransactionsController@index') }}" method="post">
+							<form  name="buySharesForm" action="{{ action('TransactionsController@buy') }}" method="post">
 								<input name="searchText" type="hidden" type="text" value="<?php echo isset($_POST['symbol']) ? $_POST['symbol'] : '' ?>">
-								<!-- 					Company : <?php echo $company ?>-->							
+								<!-- 					Company : <?php echo $company ?>-->
 								<p>Shares to Buy :<input name="sharePrice" type="hidden" id="sharePrice" value="<?php echo $price ?>" >
 									<input name="numberOfSharesBuy" id="numberOfSharesBuy" style="width: 4.5em" placeholder="#" type="number" min="1" step="1"
 									onchange="calculateTotalShareCostBuy(this)" disabled> </p>
@@ -140,11 +141,11 @@ $(document).ready(function(){
 									<br>
 									<button class="submitButt" id="buySharesButton" type="submit" value="submit" disabled>Buy Shares</button>
 									<br><br>
-									{{ csrf_field() }}	
+									{{ csrf_field() }}
 								</form>
 								<br>
 							</div>
-						</section>		
+						</section>
 					</div><!--Buy box-->
 
 					<!--Loads menu with all owned shares for selling-->
@@ -152,7 +153,7 @@ $(document).ready(function(){
 						<section>
 							<div id="left">
 								<form  name="APIsearchForm" onsubmit="return false";>
-									<!--TODO: Load in owned shares-->						
+									<!--TODO: Load in owned shares-->
 									<select name="ownedShares" style=" width:513px">
 										 <option value="" disabled selected hidden>Select a company...</option>
 										<option value="1">TODO: Load companies</option>
@@ -165,24 +166,24 @@ $(document).ready(function(){
 								<br>
 								<p>Or search by company symbol</p>
 								<!--TODO: Search box to use Ajax function-->
-								<form  name="APIsearchForm" action="{{ action('TransactionsController@index') }}" method="post"> 
-									<input name="symbol" placeholder="eg. ASX.AX" type="text" style="width: 250px" value="<?php echo isset($_POST['symbol']) ? $_POST['symbol'] : '' ?>"> 
+								<form  name="APIsearchForm" action="{{ action('TransactionsController@sell') }}" method="post">
+									<input name="symbol" placeholder="eg. ASX.AX" type="text" style="width: 250px" value="<?php echo isset($_POST['symbol']) ? $_POST['symbol'] : '' ?>">
 									<button class="submitButt" type="submit" value="submit">Search</button>
 									{{ csrf_field() }}
 								</form>
 							</div>
-							
+
 							<div id="right">
 								<form  name="sellSharesForm" action="{{ action('TransactionsController@index') }}" method="post">
 									<!-- <p>Company : <?php echo $company ?></p> -->
 									<p>Shares to Sell :<input name="sharePrice" type="hidden" id="sharePrice" value="<?php echo $price ?>" >
 										<input name="numberOfSharesSell" id="numberOfSharesSell" style="width: 4.5em"  placeholder="#" type="number" min="1" step="1"
-										onchange="calculateTotalShareCostSell(this)" disabled></p> 
-										<p>Total Value: $<input name="totalCostOfSharesSell" style="width: 6em; border: 0" id="totalCostOfSharesSell" value="" readonly> <?php echo $currency ?></p> 
+										onchange="calculateTotalShareCostSell(this)" disabled></p>
+										<p>Total Value: $<input name="totalCostOfSharesSell" style="width: 6em; border: 0" id="totalCostOfSharesSell" value="" readonly> <?php echo $currency ?></p>
 										<br>
 										<button class="submitButt" id="sellSharesButton" type="submit" value="submit" disabled>Sell Shares</button>
 									<br><br>
-									{{ csrf_field() }}	
+									{{ csrf_field() }}
 								</form>
 									<br>
 								</div>
@@ -192,12 +193,12 @@ $(document).ready(function(){
 						<div class="stock box" style="display:none">
 							<hr class="style1">
 							<h3><b>Stockmarket Information</b></h3>
-							<p> <span id="companyData"></span></p> 
+							<p> <span id="companyData"></span></p>
 							<p>TODO: Chart to go here</p><br>
 							<!--TODO: chart to be refreshed to currently selected company-->
 							<!--<div id="chart_div" style="width: 100%; height: 250px;"></div>-->
-							
-					<!--new table formated				
+
+					<!--new table formated
 							<table style="width:30%">
 								<tr>
 									<th><h5>Company:</h5></th>
@@ -215,10 +216,10 @@ $(document).ready(function(){
 									<th><h5>Change:</h5></th>
 									<td>$<?php echo $change ?></td>
 								</tr>
-								
+
 							</table>
-					//new table-->			
-							
+					//new table-->
+
 						</div>
 					</div>
 				</div>
