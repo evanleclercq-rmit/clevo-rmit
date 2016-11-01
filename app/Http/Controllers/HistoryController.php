0d5c2ca8d87b0f0c97bb92f2dc125d7b219f0810
Lucas Brook <s3229113@student.rmit.edu.au>
2016-11-02 00:37:21 +1000

    replaced company page with history page

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -5,13 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 
-class CompanyController extends Controller
+class HistoryController extends Controller
 {
     
     // initial landing function for the dashboard
     public function index () {
      
-    $companies = \App\Companies::pluck('name', 'symbol');
-    return view('company')->with('companies', $companies);
+    return view('history');
 }
 }
---
+++ resources/views/history.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<div id="body">	
+	<div class="container">
+		<div class="col-md-12 content-left">
+			<div class="contact-form wow fadeInUp animated">
+			<h3><b>Transaction History</b></h3>
+			<p>List a summary of transactions within specified dates</p>
+						</div><!--contact-form-->
+					</div><!--col-md-12 content-left-->
+				</div><!--container-->
+			</div> <!--body-->
+
+			@endsection
