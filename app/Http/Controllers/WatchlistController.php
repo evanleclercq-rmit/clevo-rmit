0352fb5350b5d18d69be7d6d633a7a49e60547ba
VIOWINMBP\dashony <s3345368@student.rmit.edu.au>
2016-11-06 15:12:13 +1100

    replaced "\" with "/" and now the add and remove from favourites on the online version woks fine

--- app/Http/Controllers/WatchlistController.php
+++ app/Http/Controllers/WatchlistController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use Illuminate\Support\Facades\Auth;
-require(app_path().'\Watchlist.php');
+require(app_path().'/Watchlist.php');
 
 
 class WatchlistController extends Controller
