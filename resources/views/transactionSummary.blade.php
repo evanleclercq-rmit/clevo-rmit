90f9c38b906728215c50c955f927ebb5bbf5bc17
Evan Le Clercq <s3516635@student.rmit.edu.au>
2016-11-07 23:18:20 +1030

    moved summary details into a table

--- resources/views/transactionSummary.blade.php
+++ resources/views/transactionSummary.blade.php
@@ -7,11 +7,14 @@
     <?php
 
         $feeExplain = "";
+        $sharesDesc = "";
 
         if ($info['transaction'] == "Sale") {
             $feeExplain = "    ($50 Transaction fee plus 0.25% of sale price)";
+            $sharesDesc = "Number of Shares Sold: ";
         } else {
             $feeExplain = "    ($5o Transaction fee plus 1% of purchase price)";
+            $sharesDesc = "Number of Shares Purchased: ";
         }
 
         function determineTransaction ($info) {
@@ -29,22 +32,51 @@
             <div class="col-md-12 content-left">
                 <div class="contact-form wow fadeInUp animated">
                     <h3><b>Transaction Summary</b></h3><br>
-                    <?php echo ("Transaction Type: " . $info['transaction']); ?>
-                    <br />
-                    <?php echo ("Company: " . $info['companySymbol'] . " - " . $info['companyName']); ?>
-                    <br />
-                    <?php echo (determineTransaction($info)); ?>
-                    <br />
-                    <?php echo ("Unit Price: $" . $info['price']); ?><i> to add 1% during purchase or deduct 0.25% during sell </i>
-                    <br /><br />
-                    <?php echo ("Starting Balance: $" . $info['startBalance']); ?>
-                    <br />
-                    <?php echo ("Transaction Cost: $" . $info['totalCost']); ?>
-					<br />
-                    <?php echo ("Transaction Fees: $" . $info['fee'] . $feeExplain); ?>
-                    <br />
-                    <?php echo ("Closing Balance: $" . $info['closeBalance']); ?>
-                    <br /> <br />
+
+                    <table id = "summaryTable">
+
+                        <tr>
+                            <td>Company Symbol: </td>
+                            <td><?php echo ($info['companySymbol']); ?></td>
+                        </tr>
+
+                        <tr>
+                            <td>Company Name: </td>
+                            <td><?php echo($info['companyName']); ?></td>
+                        </tr>
+                        <tr>
+                            <td><?php echo ($sharesDesc) ?></td>
+                            <td><?php echo ($info['numberShares']); ?></td>
+                        </tr>
+
+                        <tr>
+                            <td>Unit Price: </td>
+                            <td><?php echo ("$" . $info['price']); ?></td>
+                        </tr>
+
+                        <tr>
+                            <td>Starting Balance: </td>
+                            <td><?php echo ("$" . $info['startBalance']); ?></td>
+                        </tr>
+
+                        <tr>
+                            <td>Transaction Cost: </td>
+                            <td><?php echo ("$" . $info['totalCost']); ?></td>
+                        </tr>
+
+                        <tr>
+                            <td>Transaction Fees: </td>
+                            <td><?php echo ("$" . $info['fee']); ?></td>
+                        </tr>
+
+                        <tr>
+                            <td>Closing Balance: </td>
+                            <td><?php echo ("$" . $info['closeBalance']); ?></td>
+                        </tr>
+
+                    </table>
+
+                    <br><br>
 
                     <form action = "{{ url('/dashboard') }}" method = "POST" style = "display:inline-block">
                         <button type="submit" class="btn btn-primary">Return to Dashboard</button>
