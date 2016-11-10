68f09875deb60530559d7d046f0de130c33af1de
Lucas Brook <s3229113@student.rmit.edu.au>
2016-11-10 11:49:37 +1000

    added total cost of transaction

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use Illuminate\Support\Facades\Auth;
+use App\Transaction; 
+
 require(app_path().'/DatabaseUtilities.php');
 
 class TransactionsController extends Controller
@@ -12,8 +14,8 @@ class TransactionsController extends Controller
 
     // initial landing function for the transactions
     public function index () {
-        $companies = \App\Companies::pluck('name', 'symbol');
-    	return view('transactions')->with('companies', $companies);
+      $companies = \App\Companies::pluck('name', 'symbol');
+      return view('transactions')->with('companies', $companies);
     }
 
     public function buy (Request $request) {
@@ -47,22 +49,24 @@ class TransactionsController extends Controller
         updateHoldings($user->id, $holdings);
 
         // // //Updating the Users Balance
-        $total = $price * $numberPurchased;
-        $fee = ($total * 0.01) + 50;
+        $shareValue = $price * $numberPurchased;
+        $fee = ($shareValue * 0.01) + 50;
+        $total = $shareValue + $fee;
         // echo ('<br><br> Total:'.$total);
         $newBalance = $user->balance - ($total + $fee);
         updateBalance($user->id, $newBalance);
-
+        
         $info = array (
                        'transaction'=>'Purchase',
                        'companySymbol'=>$companySymbol,
                        'companyName'=>$companyName,
                        'numberShares'=>$numberPurchased,
-                       'price'=>$price,
-                       'totalCost'=>$total,
-                       'closeBalance'=>$newBalance,
-                       'startBalance'=>$user->balance,
-                       'fee'=>$fee
+                       'price'=>number_format((float)$price, 2, '.', ''),
+                       'shareValue'=>number_format((float)$shareValue, 2, '.', ''),
+                      'totalCost'=>number_format((float)$total, 2, '.', ''),
+                       'closeBalance'=>number_format((float)$newBalance, 2, '.', ''),
+                       'startBalance'=>number_format((float)$user->balance, 2, '.', ''),
+                       'fee'=>number_format((float)$fee, 2, '.', ''),
                        );
 
         return view ('transactionSummary', ['info'=>$info]);
@@ -96,11 +100,12 @@ class TransactionsController extends Controller
 
         updateHoldings($user->id, $holdings);
 
-        $total = $price * $numberPurchased;
-
+        $shareValue = $price * $numberPurchased;
         //Calculate fee of 0.25% of sale
-        $fee = ($total * 0.0025) + 50;
-        $newBalance = $user->balance + ($total - $fee);
+        $fee = ($shareValue * 0.0025) + 50;
+        $total = $shareValue - $fee;
+
+        $newBalance = $user->balance + ($shareValue - $fee);
 
         updateBalance($user->id, $newBalance);
 
@@ -110,17 +115,18 @@ class TransactionsController extends Controller
 
         $info = array (
                        'transaction'=>'Sale',
-                       'companySymbol'=>$companySymbol,
+                      'companySymbol'=>$companySymbol,
                        'companyName'=>$companyName,
                        'numberShares'=>$numberPurchased,
-                       'price'=>$price,
-                       'totalCost'=>$total,
-                       'startBalance'=>$user->balance,
-                       'closeBalance'=>$newBalance,
-                       'fee'=>$fee
+                       'price'=>number_format((float)$price, 2, '.', ''),
+                       'shareValue'=>number_format((float)$shareValue, 2, '.', ''),
+                        'totalCost'=>number_format((float)$total, 2, '.', ''),
+                       'closeBalance'=>number_format((float)$newBalance, 2, '.', ''),
+                       'startBalance'=>number_format((float)$user->balance, 2, '.', ''),
+                       'fee'=>number_format((float)$fee, 2, '.', ''),
                        );
 
         return view ('transactionSummary', ['info'=>$info]);
-    }
-
+    } 
+ 
 }
--- resources/views/transactionSummary.blade.php
+++ resources/views/transactionSummary.blade.php
@@ -12,9 +12,12 @@
         if ($info['transaction'] == "Sale") {
             $feeExplain = "    ($50 Transaction fee plus 0.25% of sale price)";
             $sharesDesc = "Number of Shares Sold: ";
+            $costOrProfit = "Total profit: ";
         } else {
             $feeExplain = "    ($5o Transaction fee plus 1% of purchase price)";
             $sharesDesc = "Number of Shares Purchased: ";
+            $costOrProfit = "Total cost: ";
+
         }
 
         function determineTransaction ($info) {
@@ -33,44 +36,50 @@
                 <div class="contact-form wow fadeInUp animated">
                     <h3><b>Transaction Summary</b></h3><br>
 
-                    <table id = "summaryTable">
+                    <table id = "summaryTable" style="width:60%">
 
                         <tr>
-                            <td>Company Symbol: </td>
+                            <td><b>Company Name: </td>
+                            <td><?php echo($info['companyName']); ?></td>
+                        </tr>
+
+                        <tr>
+                            <td><b>Company Symbol: </td>
                             <td><?php echo ($info['companySymbol']); ?></td>
                         </tr>
 
                         <tr>
-                            <td>Company Name: </td>
-                            <td><?php echo($info['companyName']); ?></td>
+                            <td><b>Starting Balance: </td>
+                            <td><?php echo ("$" . $info['startBalance']); ?></td>
                         </tr>
-                        <tr>
-                            <td><?php echo ($sharesDesc) ?></td>
+
+                        <tr> 
+                            <td><b><?php echo ($sharesDesc) ?></td>
                             <td><?php echo ($info['numberShares']); ?></td>
                         </tr>
 
                         <tr>
-                            <td>Unit Price: </td>
+                            <td><b>Unit Price: </td>
                             <td><?php echo ("$" . $info['price']); ?></td>
                         </tr>
 
                         <tr>
-                            <td>Starting Balance: </td>
-                            <td><?php echo ("$" . $info['startBalance']); ?></td>
+                            <td><b>Share Value: </td>
+                            <td><?php echo ("$" . $info['shareValue']); ?></td>
                         </tr>
 
                         <tr>
-                            <td>Transaction Cost: </td>
-                            <td><?php echo ("$" . $info['totalCost']); ?></td>
-                        </tr>
-
-                        <tr>
-                            <td>Transaction Fees: </td>
+                            <td><b>Transaction Fee: </td>
                             <td><?php echo ("$" . $info['fee']); ?></td>
                         </tr>
 
                         <tr>
-                            <td>Closing Balance: </td>
+                            <td><b><?php echo ($costOrProfit) ?></td>
+                            <td><?php echo ("$" . $info['totalCost']); ?></td>
+                        </tr>
+
+                        <tr>
+                            <td><b>Closing Balance: </td>
                             <td><?php echo ("$" . $info['closeBalance']); ?></td>
                         </tr>
 
