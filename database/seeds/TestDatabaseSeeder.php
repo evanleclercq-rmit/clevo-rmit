8a482acd55f3ae7ca2f3c3295171a05e2d5115b8
Lucas Brook <s3229113@student.rmit.edu.au>
2016-11-18 12:24:20 +1000

    added new values to test db seeder

--- database/seeds/TestDatabaseSeeder.php
+++ database/seeds/TestDatabaseSeeder.php
@@ -17,14 +17,16 @@ class TestDatabaseSeeder extends Seeder
      */
     public function run()
     {
-    	$faker = Faker::create();
-    	foreach (range(1,10) as $index) {
-	        DB::table('users')->insert([
-	            'name' => $faker->name,
-	            'email' => $faker->email,
-	            'password' => bcrypt('secret'),
-                'balance' => '1000000',
-	        ]);
+        $faker = Faker::create();
+        foreach (range(1,10) as $index) {
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'city' => $faker->city,
+                'email' => $faker->email,
+                'age' => $faker->numberBetween($min = 18, $max = 99),
+                'password' => bcrypt('secret'),
+                'balance' => '20000',
+            ]);
         }
     }
 }
