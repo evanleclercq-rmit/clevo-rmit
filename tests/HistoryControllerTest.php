53e02f00c46277202a199fe4597553978fe397c0
OcalOgten <s3355170@student.rmit.edu.au>
2016-11-18 07:11:53 +0000

    History controller test

--- tests/HistoryControllerTest.php
+++ tests/HistoryControllerTest.php
@@ -6,12 +6,15 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class HistoryControllerTest extends TestCase
 {
+    public $user;
 	
     public function setUp()
     {
         parent::setUp();
         Artisan::call('migrate');
         Artisan::call('db:seed', ['--class' => 'DatabaseSeeder', '--database' => 'testing']);
+
+        $this->user = factory(App\User::class)->create();
     }    
 
     public function tearDown()
@@ -22,17 +25,35 @@ class HistoryControllerTest extends TestCase
 
     public function test_database_was_seeded()
     {
-        $this->assertCount(10, user::all());
+        $this->assertCount(11, user::all());
     }
-
-	
 	
 	public function test_get_history_page()
 	{
-		$user = factory(App\User::class)->create();
-
-		$this->actingAs($user)
-			 ->visit('/')
-			 ->see('history');
+		$this->actingAs($this->user)
+			 ->visit('/history')
+			 ->see('Transaction History');
 	}
+
+    public function test_history_view_has_data()
+    {
+        $this->actingAs($this->user)
+             ->get('/history')
+             ->assertViewHas('history');
+    }
+
+    public function test_all_history_view_with_unregistered_user()
+    {
+        $response = $this->call('GET', '/history');
+
+        $this->assertEquals(302, $response->status());
+    }
+    
+    public function test_all_history_view_with_registered_user()
+    {
+        $this->actingAs($this->user)->call('GET', '/history');
+
+        $this->assertResponseOk();
+    }
+
 }
